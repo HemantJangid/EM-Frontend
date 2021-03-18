b1cf76217bf1b861af24c14080e0e3d1d0b6caae
HemantJangid <jhemant539@gmail.com>
2021-03-18 14:14:43 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -3,33 +3,15 @@ import Home from "./screens/Home";
 import About from "./screens/About";
 import Contact from "./screens/Contact";
 import Footer from "./components/Footer";
-<<<<<<< Updated upstream
 import Partner from "./screens/Partner";
-=======
-import BlogPost from "./screens/BlogPost";
-import Contact from "./screens/Contact";
-// import Blog from "./screens/Blog";
-// import Contact from "./screens/Contact";
-import Product from "./screens/Product";
-import Partner from "./screens/Partner";
-import TestRide from "./screens/TestRide";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
-<<<<<<< Updated upstream
       {/* <Home /> */}
       {/* <About /> */}
       {/* <Contact /> */}
       <Partner />
-=======
-      {/* <BlogPost /> */}
-      {/* <Product /> */}
-      <Contact />
-      {/* <TestRide /> */}
-      {/* <Partner /> */}
->>>>>>> Stashed changes
       <Footer />
     </div>
   );
